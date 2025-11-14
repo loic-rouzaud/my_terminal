96bc9ebedecda1d049e7006c4c3208659543c145
loic-rouzaud <loic.rouzaud@epitech.eu>
2025-11-14 17:22:35 +0100

    [ADD] - renderer file ++ comments

---
+++ src/renderer.rs
@@ -0,0 +1,6 @@
+// ici sera ajouté le code pour pouvoir afficher du texte dans la window elle meme
+// utiliser rusttype
+//
+// Il faut telecharger un police sympatoche.
+//
+// Mais la c'est la fin de journée donc flemme
